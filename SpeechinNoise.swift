e6f141fcba0d7680ce752cb93cad5a7f2880719c
cyli2014 <cylisjtu@gmail.com>
2019-04-09 16:18:42 -0400

    test

--- SpeechinNoise.swift
+++ SpeechinNoise.swift
@@ -10,15 +10,5 @@ import Foundation
 import ResearchKit
 
 public var SpeechinNoise: ORKOrderedTask {
-    var steps = [ORKStep]()
-    
-    let sentence1 = ORKOrderedTask.speechInNoiseTask(withIdentifier: "SpeechinNoise", intendedUseDescription: "Test", options: ORKPredefinedTaskOption())
-    steps += [sentence1]
-    
-    let sentence2 = ORKOrderedTask.speechInNoiseTask(withIdentifier: "SpeechinNoise", intendedUseDescription: "Test", options: ORKPredefinedTaskOption())
-    steps += [sentence2]
-    
-    let task = ORKNavigableOrderedTask(identifier: "speechsteps", steps: steps)
-    
-    return task
+    return ORKOrderedTask.speechInNoiseTask(withIdentifier: "SpeechinNoise", intendedUseDescription: "Test", options: ORKPredefinedTaskOption())
 }
